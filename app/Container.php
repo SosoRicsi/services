be6526fbc16e0a2434486d6ace5cf8a074ffe461
Jarkó Richárd <jarkoricsi16@gmail.com>
2024-11-23 01:02:32 +0100

    Initial commit

---
+++ app/Container.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace BasicPHP\Services;
+
+class Container 
+{
+
+	protected array $bindings = [];
+
+	public function bind(string $key, callable $resolver): void
+	{
+		$this->bindings[$key] = $resolver;
+	}
+
+	public function take($key): mixed
+	{
+		if (!array_key_exists($key, $this->bindings)) {
+			throw new \Exception("No matching binding found for [{$key}]!");
+		}
+
+		$resolver = $this->bindings[$key];
+
+		return call_user_func($resolver);
+	}
+
+}
---
+++ app/Service.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types=1);
+
+namespace BasicPHP\Services;
+
+/**
+ * Class App
+ *
+ * Az `App` osztály a framework fő belépési pontjaként szolgál,
+ * amely tárolja az alkalmazás fő konténerét és router objektumát.
+ */
+class Service
+{
+
+	/**
+	 * @var object $container Tárolja az alkalmazás konténerét, amely az egyes komponensek regisztrálására és elérésére szolgál.
+	 */
+	protected static object $container;
+
+	/**
+	 * Beállítja az alkalmazás konténerét.
+	 *
+	 * @param object $container Egy objektum, amely a konténerként szolgál.
+	 */
+	public static function setContainer(object $container): void
+	{
+		static::$container = $container;
+	}
+
+	public static function bind(string $key, callable|object $container): void
+	{
+		static::$container->bind($key, $container);
+	}
+
+	/**
+	 * Visszaad egy komponenst a konténerből az adott kulcs alapján.
+	 *
+	 * @param string $key A lekérdezett komponens kulcsa.
+	 * @return mixed A konténerből visszaadott komponens.
+	 */
+	public static function container(string $key): mixed
+	{
+		return static::$container->take($key);
+	}
+
+}
